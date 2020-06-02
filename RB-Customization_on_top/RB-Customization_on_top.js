e9e0cac9e5e9fc2261758f20e82a5e5212908500
peppemete@MacBook <mete46@hotmail.it>
2020-06-02 09:01:35 +0200

    add condition only for mobile

--- RB-Customization_on_top/RB-Customization_on_top.js
+++ RB-Customization_on_top/RB-Customization_on_top.js
@@ -1,5 +1,7 @@
+if ($(window).width() <= 767) {  
 $("#prescriptionDetailContainer").css("display","none");
 $("#prodSizeContainer").css({"display":"flex","flex-direction":"column"});
 $("#wcs-size-pdp-display").css("order","0");
 $("#wcs-pdp-buy-button-display").css("order","2");
 $("#prodSizeContainer>hr.customiseDividerLine").css("display","none");
+}
