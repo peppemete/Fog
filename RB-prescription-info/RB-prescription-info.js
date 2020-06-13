da54ae358a683cdfa942ee1ed83557f6b3767316
peppemete@MacBook <mete46@hotmail.it>
2020-06-13 14:41:46 +0200

    RB- prescription info done

---
+++ RB-prescription-info/RB-prescription-info.js
@@ -0,0 +1,5 @@
+$("#prescriptionCLP-heroBanner [data-description='PrescriptionSunViewAll_PLP']").css("font-size","1.5em");
+$("#prescriptionCLP-heroBanner [data-description='EyeViewAll_PLP']").css("font-size","1.5em");
+$("#prescriptionCLP-heroBanner header>h2").css({"font-size":"3em","line-height":"1.2"});
+$('#prescriptionCLP-heroBanner').after($('[data-element-id="X_PrescriptionCLP_6-Collection"]'));
+$('[data-element-id="X_PrescriptionCLP_6-Collection"] a>img').css("mix-blend-mode","darken");
